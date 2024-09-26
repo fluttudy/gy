95cde8c6b3f4de7ddd44dccfc2e1cf1fad25d009
flow2c <flows2sea@gmail.com>
2024-09-26 18:15:04 +0900

    weather app rebuild step1 backup

--- state_management/weather/lib/providers/weater_provider.dart
+++ state_management/weather/lib/providers/weater_provider.dart
@@ -3,7 +3,7 @@ import '../models/weather.dart';
 
 class WeatherNotifier extends StateNotifier<List<Weather>> {
   WeatherNotifier() : super([]);
-
+  
 }
 
 final weatherProvider = StateNotifierProvider<WeatherNotifier, List<Weather>>((ref) {
